3f2c8d7696e14d8b19928ae8e6ca2fedcb80f6b6
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-09-16 14:49:59 +0700

    added quiz2

---
+++ js/quiz2.js
@@ -0,0 +1,12 @@
+for(let i=0; i <=20; i++) {
+  console.log(i)
+}
+for(let i=0; i <=20; i++) {
+  if(i%2 == 0) {
+    console.log(i)
+  }
+}
+const newValue = [-1,2,3,56,6,-93,0,4].map(el => el * 2)
+const newValue2 = [10,13,44,55,89,0,12,1].filter(el => el % 2 == 0)
+console.log(newValue)
+console.log(newValue2)
