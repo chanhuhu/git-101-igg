8a23304f25ad55122ceaf15da4ea85c1e3954ad3
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-09-16 15:35:44 +0700

    added quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,7 @@
+let words = "Hello my name is Joke"
+let index = words.search("Joke")
+
+console.log(index)
+let pharse = "Well open up your mind and see like me"
+let substr = pharse.slice(5,9)
+console.log(substr)
