aa709da0c208d9a4632f8e4ac2d21616744328c4
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-09-16 13:37:54 +0700

    data types

---
+++ js/example.js
@@ -0,0 +1,18 @@
+console.log("Hello world")
+const PI = 3.14
+let name = "John"
+name = "JOKE"
+let test = "hello"
+console.log("PI", PI)
+console.log("name:", name)
+console.log(`${test} B`)
+const arr = [1,2,3]
+const person = {
+  name: "john",
+  age: 12,
+  gender: {
+    name: "male"
+  },
+  arr
+}
+console.log(person)
