8205048878521a919668146c068ae813367a9172
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-09-16 13:46:36 +0700

    completed the quiz1

---
+++ js/quiz1.js
@@ -0,0 +1,9 @@
+const person = {
+  fname: "aphimuk",
+  lname: "Yimsukpaitoon",
+  age: 20,
+  nickname: "korn",
+  hoppies: ["surf the Internet", "listening to music", "reading books"],
+  tel: "0842227445"
+}
+console.log(person)
